99dd2ffed2bbbe9d6e14dc651b36a8038019d91f
Selin Bacaz <144861936+selinAbacaz@users.noreply.github.com>
2025-09-19 22:47:06 -0400

    added csv files and various things

--- packages/database/src/seed.ts
+++ packages/database/src/seed.ts
@@ -1,32 +1,44 @@
 import { prisma } from "./client";
 
-import type { User } from "../generated/client";
+//import type { User } from "../generated/client";
+import { users, profiles, courses, assignments, submissions} from "./testData.json";
 
-const DEFAULT_USERS = [
-  // Add your own user to pre-populate the database with
-  {
-    name: "Tim Apple",
-    email: "tim@apple.com",
-  },
-] as Array<Partial<User>>;
+
 
 (async () => {
   try {
     await Promise.all(
-      DEFAULT_USERS.map((user) =>
-        prisma.user.upsert({
-          where: {
-            email: user.email!,
-          },
-          update: {
-            ...user,
-          },
-          create: {
-            ...user,
-          },
-        })
-      )
-    );
+      users.map(async(user) => {
+        await prisma.user.create({ data: {id: user.id, email: user.email, Firstname: user.Firstname, Lastname: user.Lastname, pronouns: user.pronouns} });
+
+      }
+    ));
+    await Promise.all(
+      profiles.map(async(profile) => {
+        await prisma.profile.create({ data: {id: profile.id, bio: profile.bio, userId: profile.userId} });
+
+      }
+    ));
+    await Promise.all(
+      courses.map(async(course) => {
+        await prisma.course.create({ data: {id: course.id, name: course.name} });
+
+      }
+    ));
+    await Promise.all(
+      assignments.map(async(assignment) => {
+        await prisma.assignment.create({ data: {id: assignment.id, courseId: assignment.courseId, authorId: assignment.authorId, createdAt: assignment.createdAt, fileType: assignment.fileType, published: assignment.published, title: assignment.title, updatedAt: assignment.updatedAt} });
+
+      }
+    ));
+    await Promise.all(
+      submissions.map(async(submission) => {
+        await prisma.submission.create({ data: {id: submission.id, assignmentId: submission.assignmentId, authorId: submission.authorId, fileName: submission.fileName, submittedAt: submission.submittedAt} });
+
+      }
+    ));
+
+
   } catch (error) {
     console.error(error);
     process.exit(1);
